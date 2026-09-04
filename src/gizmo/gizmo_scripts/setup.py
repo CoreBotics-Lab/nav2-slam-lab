cb92f2369a7a7c363bb68cd1cccb92a534a3b4cd
CoreBotics-Lab <hayisyed@gmail.com>
2026-09-04 07:37:00 +0000

    feat: initialize gizmo_scripts package for Nav2 Simple Commander missions

---
+++ src/gizmo/gizmo_scripts/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'gizmo_scripts'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='CoreBotics-Lab',
+    maintainer_email='hayisyed@gmail.com',
+    description='Application layer Python missions and autonomous navigation scripts for Gizmo robot using Nav2 Simple Commander API',
+    license='Apache-2.0',
+    extras_require={
+        'test': ['pytest'],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
