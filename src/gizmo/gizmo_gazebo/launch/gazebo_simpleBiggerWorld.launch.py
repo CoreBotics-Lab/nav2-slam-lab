e6ca23c14e69ae3d7d6b2d2f1362baa7411f91ff
CoreBotics-Lab <hayisyed@gmail.com>
2026-09-06 07:22:02 +0000

    feat(gazebo, navigation): add simpleBiggerWorld launch and generate 2D map
    - Add gazebo_simpleBiggerWorld.launch.py to launch Gazebo with simpleBiggerWorld.sdf.
    
    - Add saved 2D occupancy grid map (YAML and PGM) for static map navigation in simpleBiggerWorld.
    
    - Fix YAML image reference to point to simple_bigger_world_map.pgm.

---
+++ src/gizmo/gizmo_gazebo/launch/gazebo_simpleBiggerWorld.launch.py
@@ -0,0 +1,29 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+def generate_launch_description():
+    gizmo_gazebo_dir = get_package_share_directory('gizmo_gazebo')
+    
+    simple_world_path = os.path.join(
+        gizmo_gazebo_dir,
+        'worlds',
+        'simpleBiggerWorld.sdf'
+    )
+    
+    gazebo_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(gizmo_gazebo_dir, 'launch', 'gazebo.launch.py')
+        ),
+        launch_arguments={
+            'world_file': simple_world_path,
+            'use_camera': 'false',
+            'run_rviz2': 'false'
+        }.items()
+    )
+    
+    return LaunchDescription([
+        gazebo_launch
+    ])
